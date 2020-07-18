85396f0d819531b6353c463d3b493b3095c1dc18
sailordu <sailor.du@gmail.com>
2020-07-18 21:04:41 +0800

    Update build-kernel.sh

--- build-kernel.sh
+++ build-kernel.sh
@@ -28,7 +28,7 @@ KERNEL_REPO=https://github.com/friendlyarm/kernel-rockchip
 KERNEL_BRANCH=nanopi-r2-v5.4.y
 
 declare -a KERNEL_3RD_DRIVERS=("https://github.com/friendlyarm/rtl8821CU" "https://github.com/friendlyarm/rtl8822bu" "https://github.com/sailordu/rtl8812au")
-declare -a KERNEL_3RD_DRIVER_BRANCHES=("nanopi-r2" "nanopi-r2" "5.6.4.2")
+declare -a KERNEL_3RD_DRIVER_BRANCHES=("nanopi-r2" "nanopi-r2" "v5.6.4.2")
 declare -a KERNEL_3RD_DRIVER_NAME=("rtl8821CU" "rtl8822bu" "rtl8812au")
 
 ARCH=arm64
